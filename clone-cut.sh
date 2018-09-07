9fea936c79d5323ea20fdc108689cd4df33b3956
Jonathan Moallem <jdmoallem@gmail.com>
2018-09-07 14:26:39 -0400

    Initial commit

---
+++ clone-cut.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone down the repo
+git clone https://github.com/J-Mo63/aitp-labs.git
+
+# Remove the old files
+rm -r -f ./IT207
+
+# Move the contents of the repo into the current directory
+mv ./aitp-labs/IT207 .
+
+# Remove the repo
+rm -r -f ./aitp-labs/
